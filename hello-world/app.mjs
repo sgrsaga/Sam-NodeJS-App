7e428d048467989ee47fd382aca946f37fe2fee1
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-08-02 00:40:55 +0530

    Final Test project

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -14,33 +14,38 @@
 
 import AWS from 'aws-sdk';
 //const AWS = require('aws-sdk')
-/*
+
 AWS.config.update({
      region: 'ap-south-1'
  });
 
- const myProductsTable = AWS.DynamoDB.DocumentClient();
- const productTableName = 'products';
- const healthPath = '/health';
- const getProductsPath = '/products';
- const addProductsPapth = '/addProd';
- const updateProductPath = '/updateProd';
-*/
+//const myProductsTable = AWS.DynamoDB.DocumentClient();
+const productTableName = 'products';
+const healthyPath = '/health';
+const getProductsPath = '/products';
+const addProductsPapth = '/addProd';
+const updateProductPath = '/updateProd';
+
 
 export const lambdaHandler = async (event, context) => {
     try {
         //console.log(event);
         console.log(AWS.VERSION);
-        return AWS.VERSION;
-
-        /*
+      
         let response;
         switch(true){
-            case event.httpMethod === 'GET' && event.path === healthPath:
-                response = buildResponse(200);
+            case event.httpMethod === 'GET' && event.path === healthyPath:
+                console.log(healthyPath + ' -> Success')
+                response = {
+                    "statusCode": 200,
+                    "headers": {
+                      "Content-Type": "application/json"
+                    },
+                    "body": "{\"message\":\"Hello, world! - Success - healthyPath\"}"
+                  };
         }
         return response;
-        */
+        
 
     } catch (err) {
         console.log(err);
