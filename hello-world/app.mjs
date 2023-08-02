dc08abe15f86e1b5c214b570e2da151a105076f4
Jayathilaka_Sagara <sagara.jayathilaka@pearson.com>
2023-08-02 20:53:47 +0530

    TEST 1

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -10,45 +10,67 @@
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
+ import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-
-import AWS from 'aws-sdk';
-//const AWS = require('aws-sdk')
-
-AWS.config.update({
-     region: 'ap-south-1'
- });
-
-//const myProductsTable = AWS.DynamoDB.DocumentClient();
-const productTableName = 'products';
-const healthyPath = '/health';
-const getProductsPath = '/products';
-const addProductsPapth = '/addProd';
-const updateProductPath = '/updateProd';
-
-
-export const lambdaHandler = async (event, context) => {
-    try {
-        //console.log(event);
-        console.log(AWS.VERSION);
-      
-        let response;
-        switch(true){
-            case event.httpMethod === 'GET' && event.path === healthyPath:
-                console.log(healthyPath + ' -> Success')
-                response = {
-                    "statusCode": 200,
-                    "headers": {
-                      "Content-Type": "application/json"
-                    },
-                    "body": "{\"message\":\"Hello, world! - Success - healthyPath\"}"
-                  };
-        }
-        return response;
-        
-
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-};
+ const dbClient = new DynamoDBClient({ region: "ap-south-1"})
+ 
+ export const lambdaHandler = async (event, context) => {
+     try {
+         const healthyPath = '/health';
+         const getProductsPath = '/products';
+         const addProductsPapth = '/addProd';
+         const updateProductPath = '/updateProd';
+         const productTableName = 'MyProductInfo';
+ 
+ 
+         console.log(event);
+ 
+         let response;
+         switch(true){
+             case event.httpMethod === 'GET' && event.path === healthyPath:
+                 console.log(healthyPath + ' -> Success')
+                 response = {
+                     "statusCode": 200,
+                     "headers": {
+                       "Content-Type": "application/json"
+                     },
+                     "body": "{\"message\":\"Hello, world! - Success - healthyPath\"}"
+                   };
+             case event.httpMethod === "GET" && event.path === getProductsPath:
+                 console.log(`Get Tables from ptah : ${getProductsPath}`);
+                 const command = new ListTablesCommand({});
+                 const response = await dbClient.send(command);
+                 console.log(response);
+         }
+         return response;
+ 
+     } catch (err) {
+         console.log(err);
+         return err;
+     }
+ };
+ 
+ 
+ /*
+ export const main = async () => {
+   const command = new ListTablesCommand({});
+ 
+   const response = await client.send(command);
+   console.log(response);
+   return response;
+ };
+ 
+ async function getProducts() {
+ 
+     const command = new ListTablesCommand({});
+     const datalist = await dbClient.send(command);
+     console.log(datalist);
+     return {
+         "statusCode": 200,
+         "headers": {
+             "Content-Type": "application/json"
+             },
+             "body": datalist
+             };
+   }
+   */
